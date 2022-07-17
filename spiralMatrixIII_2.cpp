19b92fee9b2f442c93651a5a890462ff32ad841c
VHacker2022 <xiangrui@aiursoft.com>
2022-07-17 13:14:29 +0800

    add 螺旋矩阵III(我个人感觉三更难)

---
+++ spiralMatrixIII_2.cpp
@@ -0,0 +1,66 @@
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    vector<vector<int>> spiralMatrixIII(int rows, int cols, int rStart, int cStart) {
+        vector<vector<int>> results;
+        int cnt = rows * cols, step = 1, t = 0;
+
+        while (results.size() < rows * cols)
+        {
+            // 右
+            for (int i = 0; i < step; i++)
+            {
+                // 判断
+                if (rStart >= 0 && cStart >= 0 && rStart <= rows - 1 && cStart <= cols - 1)
+                {
+                    results.push_back({rStart, cStart});
+                }
+
+                cStart++;
+                cnt--;
+            }
+            // 下
+            for (int i = 0; i < step; i++)
+            {
+                // 判断
+                if (rStart >= 0 && cStart >= 0 && rStart <= rows - 1 && cStart <= cols - 1)
+                {
+                    results.push_back({rStart, cStart});
+                }
+                rStart++;
+
+                cnt--;
+            }
+            step++;
+            // 左
+            for (int i = 0; i < step; i++)
+            {
+                // 判断
+                if (rStart >= 0 && cStart >= 0 && rStart <= rows - 1 && cStart <= cols - 1)
+                {
+                    results.push_back({rStart, cStart});
+                }
+                cStart--;
+
+                cnt--;
+            }
+            // 上
+            for (int i = 0; i < step; i++)
+            {
+                // 判断
+                if (rStart >= 0 && cStart >= 0 && rStart <= rows - 1 && cStart <= cols - 1)
+                {
+                    results.push_back({rStart, cStart});
+                }
+                rStart--;
+
+                cnt--;
+            }
+            step++;
+        }
+
+        return results;
+    }
+};
